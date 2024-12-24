90f45e2527e727e050e833a4335d8af7ae444770
OwinoMichael <owinomichael1400@gmail.com>
2024-12-23 22:00:30 -0500

    Logging and Saving file using Simple Logging For Java package

--- src/main/java/com/example/demo/Product/commandHandlers/CreateProductCommandHandler.java
+++ src/main/java/com/example/demo/Product/commandHandlers/CreateProductCommandHandler.java
@@ -5,9 +5,12 @@ import com.example.demo.Exceptions.ProductNotValidException;
 import com.example.demo.Product.Model.Product;
 import com.example.demo.Product.ProductRepository;
 import io.micrometer.common.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class CreateProductCommandHandler implements Command <Product, ResponseEntity>{
@@ -15,9 +18,11 @@ public class CreateProductCommandHandler implements Command <Product, ResponseEn
     @Autowired
     private ProductRepository productRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(CreateProductCommandHandler.class);
+
     @Override
     public ResponseEntity execute(Product product) {
-
+        logger.info("Executing " + getClass() + "with" + product.toString());
         validateProduct(product);
 
         productRepository.save(product);
@@ -27,20 +32,24 @@ public class CreateProductCommandHandler implements Command <Product, ResponseEn
 
     private void validateProduct(Product product){
         if(StringUtils.isBlank(product.getName())){
+            logger.error("Product Not Valid Exception was thrown (Invalid Name)" + product.toString());
             throw new ProductNotValidException("Product Name cannot be empty");
         }
 
         if(StringUtils.isBlank(product.getDescription())){
+            logger.error("Product Not Valid Exception was thrown (Invalid Description)" + product.toString());
             throw new ProductNotValidException("Product description cannot be empty");
         }
 
 
         if(product.getPrice() <= 0.0){
+            logger.error("Product Not Valid Exception was thrown (Invalid Price)" + product.toString());
             throw new ProductNotValidException("Product price cannot be negative");
         }
 
 
         if(product.getQuantity() <= 0.0){
+            logger.error("Product Not Valid Exception was thrown (Invalid Quantity)" + product.toString());
             throw new ProductNotValidException("Product quantity cannot be negative");
         }
     }
