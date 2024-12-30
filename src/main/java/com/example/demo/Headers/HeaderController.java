bceb283f700d075d7cbfee95086a3b4d56b9a259
OwinoMichael <owinomichael1400@gmail.com>
2024-12-29 19:19:29 -0500

    Accept JSON and XML get request

--- src/main/java/com/example/demo/Headers/HeaderController.java
+++ src/main/java/com/example/demo/Headers/HeaderController.java
@@ -1,5 +1,8 @@
 package com.example.demo.Headers;
 
+import com.example.demo.Product.Model.Product;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HeaderController {
 
-    @GetMapping("/header")
-    public String getRegionalRequest(@RequestHeader(required = false, defaultValue = "USA") String region){
-        if(region.equals("USA")) return "BALD EAGLE FREEDOM";
+    @GetMapping(value = "/header", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Product> getProduct(){
+        Product product = new Product();
+        product.setName("myProduct");
+        product.setId(1);
+        product.setDescription("greatest product ever");
 
-        if(region.equals("CAN")) return "I bleed maple syrup";
-
-        return "Country not supported :(";
+        return ResponseEntity.ok(product);
     }
 }
